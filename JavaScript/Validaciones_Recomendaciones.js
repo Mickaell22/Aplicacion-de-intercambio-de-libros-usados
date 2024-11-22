ee4d4ec168c364d69a3cff053bdcbc9203e6742d
masm007 <marco.salazarmejia03@gmail.com>
2024-11-22 15:02:59 -0500

    form terminado y validado

--- JavaScript/Validaciones_Recomendaciones.js
+++ JavaScript/Validaciones_Recomendaciones.js
@@ -1,7 +1,8 @@
 function validarFormulario(){
     clearMsj();
     let esValido = true;
-    let expresionRegular = /^[a-zA-Z]+&/;
+    let expresionRegular = /^[a-zA-Z]+$/;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     let nombre = document.getElementById("nombres");
     if(nombre.value === ""){
         cargarMsj("El nombre es requerido", nombre);
@@ -9,7 +10,7 @@ function validarFormulario(){
     }else if(nombre.value.length < 3 ){
         cargarMsj("El minimo de caracteres es 3", nombre);//ana es un nombre corto
         esValido = false;
-    }else if(expresionRegular.test(nombre.value)){
+    }else if(!expresionRegular.test(nombre.value)){
         cargarMsj("Un nombre solo usa letras", nombre);
         esValido = false;
     }
@@ -21,31 +22,46 @@ function validarFormulario(){
     }else if(apellidos.value.length < 3 ){
         cargarMsj("El minimo de caracteres es 3", apellidos); //zea es un apellido corto
         esValido = false;
-    }else if(expresionRegular.test(apellidos.value)){
+    }else if(!expresionRegular.test(apellidos.value)){
         cargarMsj("Un nombre solo usa letras", apellidos);
         esValido = false;
     }
     //validar correo
     let correo = document.getElementById("correo");
-
-
-    //validar select (combo)
-    let estado = document.querySelector(".estados");
-    if(estado.value===""){
-        //alert("Estado civil no ha sido escogido");
-        // mensaje+="</br>Estado civil no ha sido escogido";
+    if(!regexEmail.test(correo.value)){
+        cargarMsj("El correo no es valido", correo);
         esValido = false;
     }
-    // let div = document.querySelector("#mensaje");
-    //div.textContent = mensaje; //agregar solo texto
-    //div.innerHTML = mensaje; //agregar cuailquier elemento hmtl
+    //validar titulo
+    let titulo = document.getElementById("titulo");
+    if(titulo.value === ""){
+        cargarMsj("El titulo es requerido", titulo);
+        esValido = false;
+    }else if(titulo.value.length < 2){
+        cargarMsj("El minimo de caracteres es 2", titulo);
+        esValido = false;
+    }
+    //validar select (combo)
+    let estado = document.querySelector(".categorias");
+    if(estado.value===""){
+        cargarMsj("El genero del libro no ha sido escogido", estado);
+        esValido = false;
+    }
+    //validar sinopsis
+    let desc = document.getElementById("resumen");
+        if(desc.value === ""){
+            cargarMsj("La sinopsis es requerida", desc);
+            esValido = false;
+        }else if(desc.value.length < 30){
+            cargarMsj("El minimo de caracteres es 30", desc);
+            esValido = false;
+        }
     return esValido;
 }
 function cargarMsj(mensaje, elemento){
     elemento.focus();
     let span = document.createElement("span")
     span.textContent = mensaje;
-    span.style.color = "red";
     span.style.fontSize = "9pt";
     span.classList.add("error");
     elemento.parentNode.appendChild(span);
@@ -54,6 +70,6 @@ function cargarMsj(mensaje, elemento){
 function clearMsj(){
     let arrSpan = document.querySelectorAll(".error");
     for(let i=0; i<arrSpan.length; i++){
-        arrSpan[i].remove(); //elimina elemento
+        arrSpan[i].remove(); 
     }
 }
